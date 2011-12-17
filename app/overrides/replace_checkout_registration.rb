f9965fe9c126943f7b1300e671bea4f925a3cc5d
Psi <psi.xie@gmail.com>
2011-12-17 21:31:28 +0800

    update

--- app/overrides/replace_checkout_registration.rb
+++ app/overrides/replace_checkout_registration.rb
@@ -3,18 +3,18 @@
 Deface::Override.new(:virtual_path => %q{checkout/registration},
                           :name => %q{replace_checkout_registration},
                           :replace => %q{#registration},
-                          :text => %q{<div id="checkout" role="checkout" class="clearfix">
-  <div id="title" role="title">支付</div>
+                          :text => %q{<div id="checkout" role="checkout" class="clearfix checkout">
+  <div id="title" role="title" class="title">支付</div>
   <!-- START MAIN CONTENT -->
   <div id="main-content" role="main-content" class="main-content">
     <%= render "shared/error_messages", :target => @order %>
     <div id="tabs" role="tabs" class="tabs">
       <ul>
         <li class='active'><span class="number">1</span><span class="title">创建用户</span></li>
-        <li><span class="number">2</span><span class="title">地址</span></li>
+        <li><span class="number">2</span><span class="title">地址填写</span></li>
         <li><span class="number">3</span><span class="title">快递选择</span></li>
         <li><span class="number">4</span><span class="title">支付选项</span></li>
-        <li><span class="number">5</span><span class="title">支付</span></li>
+        <li><span class="number">5</span><span class="title">订单完成</span></li>
       </ul>
     </div>
     <div id="content" role="content" class="content">
